228afcc3f21c52da0d1af6c2fd1cc48b091f036a
tkwest2014 <thomaswest@thomass-air.lan>
2022-02-14 22:09:35 -0600

    Add comments

--- lesson03/assignment/index.js
+++ lesson03/assignment/index.js
@@ -1,19 +1,26 @@
 document. addEventListener("DOMContentLoaded", function(){
+
+  //Creates variable using data from data.json
   const empJSON = '[{"name":"Paolo Maldini", "jobTitle":"Front End Developer", "company":"Web Weavers", "experience":"3 Years", "school":"University of Washington", "major":"Marketing", "email":"paolo@example.com", "linkedInUrl":"paolo@linkedinprofile.com", "codeLanguages": ["HTML", "CSS", "JavaScript", "Node", "Express"]}, {"name": "Barbara Bonansea", "jobTitle": "Software Engineer", "company":"Excellence in the Cloud", "experience":"12 years", "school":"University of Southern California", "major":"Computer Science", "email":"barbara@example.com", "linkedInUrl":"barbara.linkedinprofile.com", "codeLanguages": ["HTML", "JavaScript", "CSS", "Go"]}, {"name": "Javier Hernandez", "jobTitle": "User Experience Engineer", "company": "Web Sites and More", "experience": "5 years", "school": "Seattle University", "major": "Performing Arts", "email": "javier@example.com", "linkedInUrl": "javier.linkedinprofile.com", "codeLanguages": ["HTML", "CSS", "JavaScript", "Node", "Express"]}, {"name": "Maribel Dominguez", "jobTitle": "Front End Engineer", "company": "Bits and Bytes", "experience": "6 years", "school": "University of Washington", "major": "Mechanical Engineering", "email": "maribel@example.com", "linkedInUrl": "maribel.linkedinprofile.com", "codeLanguages": ["HTML", "CSS", "JavaScript", "Node", "Express"]}]'
   const emp = JSON.parse(empJSON)
 
+  //Outputs data to ID template-hook in html file
   const container = document.querySelector('#template-hook')
 
+  //Does a For Each on each object in the variable to create the cards
   emp.forEach(function(el){
 
+    //Formats the image, title and name
     let name = document.createElement('div')
     name.innerHTML = `
     <img src="img/headshot.jpg" class="info_headshot" alt="headshot">
     <h1 class="info_name">${el.name}</h1>
     <h2 class="info_title">${el.jobTitle}</h2>`
 
+    //Adds class to the image, title and name
     name.classList.add('card_profile')
 
+    //Formats the Employee Info
     let list = document.createElement('p')
     list.innerHTML = `
     <p class="card_company"><b><span style="font-size:1.5rem">Company: </span></b>${el.company}</p>
@@ -25,8 +32,10 @@ document. addEventListener("DOMContentLoaded", function(){
     <img src="img/linkedin.svg" class="linkedin" alt="linkedin">
     <p class="card_linkedin">${el.linkedInUrl}</p>`
 
+    //Adds class to the employee info
     list.classList.add('card_info')
 
+    //Adds class to the card as a whole and appends it all together to display on the page
     let main = document.createElement('section')
     main.append(name, list)
     main.classList.add('main')
